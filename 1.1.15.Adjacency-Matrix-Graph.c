351ac0e97573e829a9ae667a84b69e01822818a8
Kunal Dubey <142141622+kunaldubey10@users.noreply.github.com>
2025-04-28 05:10:10 +0530

    Update 1.1.15.Adjacency-Matrix-Graph.c

--- 1.1.15.Adjacency-Matrix-Graph.c
+++ 1.1.15.Adjacency-Matrix-Graph.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
-int main(){
-	int n;
-	scanf("%d",&n);
-	int arr[n][n];
-	if(n<=10 && n>=1){
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
-				scanf("%d",&arr[i][j]);
-			}
-		}
-		if(n==1){
-			for(int i=0;i<n;i++){
-				for(int j=0;j<n;j++){
-					printf("%d\n",arr[i][j]);
-				}
-				printf("\n");
-			}
-		}
-		else if(n>1){
-			for(int i=0;i<n;i++){
-					for(int j=0;j<n;j++){
-						printf("%d ",arr[i][j]);
-					}
-					printf("\n");
-				}
-		}
-	}
-	else{
-		printf("-1");
-	}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n >= 1 && n <= 10) {
+        int arr[n][n];
+
+        // Input matrix
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                scanf("%d", &arr[i][j]);
+
+        // Output matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ", arr[i][j]);  // Always print a space after the number
+            }
+            if (i != n - 1) {
+                printf("\n");  // Newline after each row except the last
+            }
+        }
+    } else {
+        printf("-1");
+    }
+
+    return 0;
 }
